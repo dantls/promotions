0d08125e55cfaf1cfc72229a832f2afb3b466765
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2021-08-29 00:20:12 -0400

    feature-3: Implemented UseEffect and UseState and install axios

--- src/components/Promotion/Card/Card.jsx
+++ src/components/Promotion/Card/Card.jsx
@@ -14,6 +14,7 @@ import {
 import CommentCard from '../Comment/Comment';
 
 export default function PromotionCard({promotion}){
+
   const {
     title,
     price,
@@ -22,7 +23,6 @@ export default function PromotionCard({promotion}){
     url,
     comments
   } = promotion;
-
   return (
     <Container>
       <CardImage 
